1a02d8d992c285e40f329fee8529f7eb9a37173f
serzilo <designkota@mail.ru>
2017-02-03 12:24:11 +0300

    js build

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,14 @@
 'use strict';
 
 var gulp = require('gulp'),
+    browserify = require('browserify'),
     watch = require('gulp-watch'),
     rigger = require('gulp-rigger'),
+    args = require('yargs').argv,
+    gulpif = require('gulp-if'),
+    uglify = require('gulp-uglify'),
+    source = require('vinyl-source-stream'),
+    buffer = require('vinyl-buffer'),
     browserSync = require("browser-sync"),
     reload = browserSync.reload;
 
@@ -18,13 +24,17 @@ var config = {
 
 var path = {
     build: {
-        html: 	 'build/'
+        html: 'build/',
+        js:   'build/js/',
+        jsName: 'bundle.js'
     },
     src: {
-        html:    'src/html/index.html'
+        html:  'src/html/index.html',
+        js:    'src/js/app.js'
     },
     watch: {
-        html:    'src/html/**/*.html'
+        html:    'src/html/**/*.html',
+        js:      'src/js/**/*.js'
     }
 };
 
@@ -35,13 +45,44 @@ gulp.task('html:build', function () {
         .pipe(reload({stream: true}));
 });
 
+gulp.task('js:build', function () {
+    /*
+     gulp --env dev
+     gulp --env prod
+     gulp // default dev
+     */
+    var env = args.env || 'dev',
+        settings = {
+            dev: {
+                browserify: {debug: true}
+            },
+            prod: {
+                browserify: {}
+            }
+        };
+
+
+    return browserify(path.src.js, settings[env].browserify)
+        .bundle()
+        .pipe(source(path.build.jsName))
+        .pipe(buffer())
+        .pipe(gulpif(env != 'dev', uglify()))
+        .pipe(gulp.dest(path.build.js))
+        .pipe(reload({stream: true}));
+});
+
 gulp.task('build', [
-    'html:build'
+    'html:build',
+    'js:build'
 ]);
 
 gulp.task('watch', function(){
     watch([path.watch.html], function(event, cb) {
         gulp.start('html:build');
+    });
+
+    watch([path.watch.js], function(event, cb) {
+        gulp.start('js:build');
     });
 });
 
